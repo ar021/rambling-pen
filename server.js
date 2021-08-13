d726fc46bec65365d0eaacc1695b7da1525a77dc
Avesh Rathod <avesh@Aveshs-MacBook-Air.local>
2021-08-13 00:49:43 -0400

    added procfile

--- server.js
+++ server.js
@@ -12,10 +12,13 @@ var usersRouter = require("./routes/api/users");
 
 var app = express();
 
-// app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "build")));
+
+// app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "build")));
+}
 
 // api routes must be before the "catch all" route
 app.use("/api/posts", postsRouter);
